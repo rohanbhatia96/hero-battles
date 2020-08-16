e8aa041b7ad217210ee6da065ac3a95176717964
Rohan Bhatia <rohanbhatia96@outlook.com>
2020-08-16 08:38:07 +0530

    Login page beautification

--- web/src/app/containers/LoginForm.tsx
+++ web/src/app/containers/LoginForm.tsx
@@ -1,9 +1,12 @@
 import { useLazyQuery } from "@apollo/client";
 import { Formik, FormikHelpers } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { LOGIN_USER } from "../api/gqlQueries";
 import userLoginSchema from "../schemas/userLoginSchema";
 import { RootState } from "../store/types/reducers";
@@ -20,6 +23,10 @@ const LoginForm: React.FC = () => {
   const authToken = useSelector(
     (state: RootState) => state.loginStateReducer.authToken
   );
+  const [isModal, setIsModal] = useState<boolean>(false);
+  const handleModalClose = () => {
+    setIsModal(false);
+  };
   const dispatch = useDispatch();
   const onCompleted = () => {
     dispatch({ type: "SET_AUTH_TOKEN", payload: data?.login.authToken });
@@ -29,6 +36,7 @@ const LoginForm: React.FC = () => {
     //TODO: log error to the server
     console.log("Snap! Theres an error");
     console.log(error?.message);
+    setIsModal(true);
   };
   const initialFormValues: FormValues = {
     email: "",
@@ -49,59 +57,90 @@ const LoginForm: React.FC = () => {
     actions.setSubmitting(false);
   };
   return (
-    <>
-      <Formik
-        initialValues={initialFormValues}
-        onSubmit={submitLoginForm}
-        validationSchema={userLoginSchema}
-      >
-        {({ handleChange, handleSubmit, errors, isSubmitting, isValid }) => (
-          <Form
-            noValidate
-            onSubmit={(e) => {
-              handleSubmit(e as any);
-            }}
+    <Col xs={12}>
+      <Col xs={12} className="form-container py-4 px-4">
+        <Col xs={12} className="text-center">
+          <p className="login-upper-header">Superhero Battles</p>
+          <p className="login-upper-subtext">Sign in to continue</p>
+        </Col>
+        <Col xs={12} className="py-3">
+          <Formik
+            initialValues={initialFormValues}
+            onSubmit={submitLoginForm}
+            validationSchema={userLoginSchema}
           >
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                onChange={handleChange}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-              <Form.Text className="text-danger">
-                &#x200B;{errors.email}
-              </Form.Text>
-            </Form.Group>
+            {({
+              handleChange,
+              handleSubmit,
+              errors,
+              isSubmitting,
+              isValid,
+            }) => (
+              <Form
+                noValidate
+                onSubmit={(e) => {
+                  handleSubmit(e as any);
+                }}
+              >
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                  <Form.Text className="text-danger">
+                    &#x200B;{errors.email}
+                  </Form.Text>
+                </Form.Group>
 
-            <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={handleChange}
-              />
-              <Form.Text className="text-danger">
-                &#x200B;{errors.password}
-              </Form.Text>
-            </Form.Group>
-            <Button
-              type="submit"
-              disabled={isSubmitting || loading || !isValid}
-            >
-              Submit
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-danger">
+                    &#x200B;{errors.password}
+                  </Form.Text>
+                </Form.Group>
+                <Button
+                  type="submit"
+                  disabled={isSubmitting || loading || !isValid}
+                  block
+                >
+                  <span className="login-button-text">Sign In</span>
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Col>
+      <Col xs={12} className="my-4 text-center">
+        <Link to="/register">Don't have an account? Register here.</Link>
+        {error && <p>error: {error.message}</p>}
+        <p>isLoggedIn(redux): {JSON.stringify(isLoggedIn)}</p>
+        <p>authToken(redux): {JSON.stringify(authToken)}</p>
+        <p>data: {JSON.stringify(data)}</p>
+      </Col>
+      <Col xs={12}>
+        <Modal show={isModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Snap! There's an error</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{error?.message}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleModalClose}>
+              Try Again
             </Button>
-          </Form>
-        )}
-      </Formik>
-      {error && <p>error: {JSON.stringify(error.message)}</p>}
-      <p>isLoggedIn(redux): {JSON.stringify(isLoggedIn)}</p>
-      <p>authToken(redux): {JSON.stringify(authToken)}</p>
-      <p>data: {JSON.stringify(data)}</p>
-    </>
+          </Modal.Footer>
+        </Modal>
+      </Col>
+    </Col>
   );
 };
 
--- web/src/app/pages/Login.tsx
+++ web/src/app/pages/Login.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import LoginForm from "../containers/LoginForm";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 
 const Login: React.FC = () => {
-  return <LoginForm />;
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs={6} className="mt-5">
+          <LoginForm />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
