34a33e2eb650eff9bb8949248c8b3e4582af16a4
Rohan Bhatia <rohanbhatia96@outlook.com>
2020-08-29 00:14:02 +0530

    Added new query: isCharacterAdded

--- server/src/resolvers/user/generalResolver.ts
+++ server/src/resolvers/user/generalResolver.ts
@@ -1,5 +1,5 @@
 import { ApolloError } from "apollo-server-express";
-import { Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
 import { User } from "../../entities";
 import { isAuth } from "../../middlewares/isAuth";
 import { IContext } from "../../types/IContext";
@@ -16,6 +16,32 @@ export default class GeneralResolver {
       });
       if (user) {
         return user;
+      } else {
+        throw "Unexpected error occured.";
+      }
+    } catch (err) {
+      throw new ApolloError(err);
+    }
+  }
+
+  @Query(() => Boolean)
+  @UseMiddleware(isAuth)
+  async isCharacterAdded(
+    @Ctx() context: IContext,
+    @Arg("characterId") characterId: Number
+  ): Promise<Boolean> {
+    try {
+      const user: User | undefined = await User.findOne({
+        where: { id: context.userId },
+        relations: ["characters"],
+      });
+      if (user && user.characters) {
+        const char = user.characters.filter((c) => c.id === characterId);
+        if (char.length === 1) {
+          return true;
+        } else {
+          return false;
+        }
       } else {
         throw "Unexpected error occured.";
       }
