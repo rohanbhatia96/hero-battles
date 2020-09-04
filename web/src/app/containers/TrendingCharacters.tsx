78a65b883eaa94cae99a7105bd77b4526c274190
Rohan Bhatia <rohanbhatia96@outlook.com>
2020-09-04 15:49:26 +0530

    Improved home page styling

--- web/src/app/containers/TrendingCharacters.tsx
+++ web/src/app/containers/TrendingCharacters.tsx
@@ -5,6 +5,7 @@ import { Query, PowerStats } from "../types/graphql";
 import CharacterProfile from "../components/CharacterProfile";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 const TrendingCharacters: React.FC<{}> = () => {
   const { loading, error, data } = useQuery<Query>(GET_TRENDING_CHARACTERS);
@@ -22,12 +23,19 @@ const TrendingCharacters: React.FC<{}> = () => {
     return Math.floor(sum / 6);
   };
   return (
-    <Row className="mt-4">
+    <Row className="mt-4 flex-grow-1">
+      {loading && (
+        <Col className="d-flex justify-content-center align-items-center">
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </Col>
+      )}
       {!loading &&
         !error &&
         data &&
         data.getTrendingCharacters.map((character) => (
-          <Col xs={12} md={6} key={character.name}>
+          <Col xs={12} md={12} lg={6} key={character.name}>
             <CharacterProfile
               id={character.id}
               name={character.name}
