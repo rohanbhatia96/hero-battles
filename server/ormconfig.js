11464f7d03fd2296ac10a449aac484bcdc252868
Rohan Bhatia <rohanbhatia96@outlook.com>
2020-09-08 17:13:53 +0530

    Converted string to boolean for env variable

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,27 +1,21 @@
 module.exports = {
-   "type": "postgres",
-   "host": process.env.DB_HOST,
-   "port": process.env.DB_PORT,
-   "database": process.env.DB_NAME,
-   "username": process.env.DB_USER,
-   "password": process.env.DB_PWD,
-   "extra": {
-      "ssl": true
-   },
-   "synchronize": true,
-   "logging": process.env.DB_ISLOGGING,
-   "entities": [
-      "src/entities/*.*"
-   ],
-   "migrations": [
-      "src/migration/**/*.ts"
-   ],
-   "subscribers": [
-      "src/subscriber/**/*.ts"
-   ],
-   "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration",
-      "subscribersDir": "src/subscriber"
-   }
-}
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  extra: {
+    ssl: true,
+  },
+  synchronize: true,
+  logging: JSON.parse(process.env.DB_ISLOGGING),
+  entities: ["src/entities/*.*"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
